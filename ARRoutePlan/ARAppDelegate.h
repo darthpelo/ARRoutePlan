81eefabca19fec4489ba7aa4ba3fbd055e2a40ff
darthpelo <darthpelo@gmail.com>
2013-11-12 21:12:17 +0100

    Firs commit

---
+++ ARRoutePlan/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  ARRoutePlan
+//
+//  Created by Alessio Roberto on 12/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARRoutePlan/ARViewController.m
@@ -0,0 +1,29 @@
+//
+//  ARViewController.m
+//  ARRoutePlan
+//
+//  Created by Alessio Roberto on 12/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import "ARViewController.h"
+
+@interface ARViewController ()
+
+@end
+
+@implementation ARViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
