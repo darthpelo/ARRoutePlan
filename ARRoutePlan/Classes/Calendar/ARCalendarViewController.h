dc51ece6d50ac618097d4c03b5816454e012a99b
darthpelo <darthpelo@gmail.com>
2013-11-14 00:17:22 +0100

    Added MNCalender proj.

---
+++ ARRoutePlan/Classes/Calendar/ARCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARCalendarViewController.h
+//  ARRoutePlan
+//
+//  Created by Alessio Roberto on 13/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARCalendarViewController : UIViewController
+
+@end
---
+++ ARRoutePlan/Classes/Calendar/ARCalendarViewController.m
@@ -0,0 +1,38 @@
+//
+//  ARCalendarViewController.m
+//  ARRoutePlan
+//
+//  Created by Alessio Roberto on 13/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import "ARCalendarViewController.h"
+
+@interface ARCalendarViewController ()
+
+@end
+
+@implementation ARCalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
