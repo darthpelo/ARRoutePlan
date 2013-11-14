75639a33087652a6967e6470d654fbb0b4d23203
darthpelo <darthpelo@gmail.com>
2013-11-14 11:02:50 +0100

    UI fix (main view controller and calendar view controller)

--- ARRoutePlan/Classes/Calendar/ARCalendarViewController.m
+++ ARRoutePlan/Classes/Calendar/ARCalendarViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ARCalendarViewController.h"
+#import "UIControl+MTControl.h"
 
 @interface ARCalendarViewController ()
 
@@ -28,12 +29,28 @@
     [super viewDidLoad];
     [self.view setBackgroundColor:UIColorFromRGB(0x427CA1)];
     
-    MNCalendarView *calendarView = [[MNCalendarView alloc] initWithFrame:self.view.bounds];
+    CGRect calendarFrame = CGRectMake(0, 64, 320, self.view.bounds.size.height - 64);
+    MNCalendarView *calendarView = [[MNCalendarView alloc] initWithFrame:calendarFrame];
     calendarView.calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     calendarView.selectedDate = [NSDate date];
     calendarView.delegate = self;
     calendarView.backgroundColor = UIColor.whiteColor;
     [self.view addSubview:calendarView];
+    
+    UIButton *cancellBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    cancellBtn.frame = CGRectMake(260, 20, 60, 44);
+    [cancellBtn setTitle:@"Close" forState:UIControlStateNormal];
+    [cancellBtn touchUpInside:^(UIEvent *event) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
+    cancellBtn.tintColor = [UIColor whiteColor];
+    [self.view addSubview:cancellBtn];
+    
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 20, 200, 44)];
+    titleLabel.text = @"Choose the day";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:titleLabel];
 }
 
 - (void)didReceiveMemoryWarning
@@ -48,7 +65,7 @@
     NSTimeInterval secondsInOneHour= 1 * 60 * 60;
     NSDate *dateOneHourAhead = [date dateByAddingTimeInterval:secondsInOneHour];
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"MM/dd/yy"];
+    [formatter setDateFormat:@"MM-dd-yyyy"];
     
     NSString *stringFromDate = [formatter stringFromDate:dateOneHourAhead];
     
